50cde9391836b3c8c8c2e90c9ab76369ed0d5cfb
PlannerAndy <166178466+PlannerAndy@users.noreply.github.com>
2025-06-15 01:02:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -53,5 +53,4 @@ def download(bot_id):
     return send_from_directory("static/bots", bot["filename"], as_attachment=True)
 
 if __name__ == "__main__":
-   port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+   app.run(debug=True)
